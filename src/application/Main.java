91921a71a48ff851e8a37001ec09f85c563267af
Aldrin Ricardo <aldrinricardo96@gmail.com>
2023-01-22 17:55:53 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,12 +9,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			primaryStage.setScene(mainScene);
@@ -27,5 +30,9 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 }
